0b2a143af5b775d7265a64742a96b34d68fb6b00
gui-olivei <61833126+gui-olivei@users.noreply.github.com>
2020-03-05 10:57:42 -0300

    Update CONT1AULA0503.sql

--- CONT1AULA0503.sql
+++ CONT1AULA0503.sql
@@ -15,12 +15,3 @@ INSERT INTO tbalunos (
 VALUES (201, "Ana Silva", "F", "ana@gmail.com", "2000-05-10"),
 	   (202, "Pedro Sousa", "M", "pedro@uol.com.br", "199-09-14");
 SELECT * FROM faculdade.tbalunos;
-
-USE faculdade;
-
-CREATE TABLE tbCursos (
-idCurso INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(30) NOT NULL,
-duracao INT NOT NULL,
-periodo VARCHAR(30) NOT NULL
-);
